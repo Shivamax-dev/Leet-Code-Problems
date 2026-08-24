551f424e74cf23532ed3384f9d6eb13cdc8fb52c
Shivam Maurya <124660245+Shivamax-dev@users.noreply.github.com>
2026-08-24 13:54:06 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.45%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0; // Pointer for s
+        int j = 0; // Pointer for t
+
+        while (i < s.length() && j < t.length()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+};
